756ef09a19b4e3700bd6fb239e80428a90fd33fe
roy <jasonailu87@hotmail.com>
2022-07-21 21:43:02 +0800

    chore: init

---
+++ .eslintrc.cjs
@@ -0,0 +1,12 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
+  extends: [
+    '@antfu',
+    // "plugin:vue/vue3-essential",
+    // "eslint:recommended",
+    // "@vue/eslint-config-typescript/recommended"
+  ],
+}
